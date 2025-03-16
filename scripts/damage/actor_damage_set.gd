a87e312679fd0d02ddb2798cea40ddf09755f9f2
KOFLazycat <KOFLAzycat@163.com>
2025-03-16 23:49:23 +0800

    1. 增加怪物血条

---
+++ scripts/damage/actor_damage_set.gd
@@ -0,0 +1,18 @@
+## 角色收到伤害时的处理组件
+class_name ActorDamageSet
+extends ActorDamage
+
+@export var juicy_bar:JucyBar
+
+var health_resource:HealthResource
+
+func _ready()->void:
+	super._ready()
+	assert(juicy_bar != null)
+	health_resource = resource_node.get_resource("health")
+	juicy_bar.set_default_values(0.0, health_resource.hp, health_resource.hp)
+
+
+func _play_damaged()->void:
+	super._play_damaged()
+	juicy_bar.change_current_value(health_resource.hp)
---
+++ scripts/ui/juicy_bar/juicy_bar.gd
@@ -0,0 +1,41 @@
+## 血量条
+class_name JucyBar 
+extends Control
+
+@export var top_layer_bar: ProgressBar
+@export var bottom_layer_bar: ProgressBar
+
+@export var min_value: float
+@export var max_value: float
+@export var current_value: float
+@export var top_layer_bar_time: float = 0.2
+@export var top_layer_bar_delay: float = 0.0
+@export var bottom_layer_bar_time: float = 0.2
+@export var bottom_layer_bar_delay: float = 0.0
+
+
+func _ready() -> void:
+	current_value = max_value
+	set_default_values(min_value, max_value, current_value)
+
+
+## 直接设置默认值
+func set_default_values(min: float = 0, max: float = 100, current: float = 100) -> void:
+	top_layer_bar.min_value = min
+	top_layer_bar.max_value = max
+	top_layer_bar.value = current
+	
+	bottom_layer_bar.min_value = min
+	bottom_layer_bar.max_value = max
+	bottom_layer_bar.value = current
+
+
+func change_current_value(value: float) -> void:
+	current_value = clamp(value, min_value, max_value)
+	run_juicy_tween(top_layer_bar, current_value, top_layer_bar_time, top_layer_bar_delay)
+	run_juicy_tween(bottom_layer_bar, current_value, bottom_layer_bar_time, bottom_layer_bar_delay)
+
+
+func run_juicy_tween(bar: ProgressBar, value: float, length: float, delay: float) -> void:
+	var tween = create_tween()
+	tween.tween_property(bar, "value", value, length).set_delay(delay).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
