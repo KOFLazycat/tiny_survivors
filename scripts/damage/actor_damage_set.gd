a7cbd43306736d5026a4f737cb239efbebc3a368
KOFLAzycat <lushoulei0552@163.com>
2025-03-17 16:50:58 +0800

    1. 取消怪物血量UI显示

--- scripts/damage/actor_damage_set.gd
+++ scripts/damage/actor_damage_set.gd
@@ -8,11 +8,13 @@ var health_resource:HealthResource
 
 func _ready()->void:
 	super._ready()
-	assert(juicy_bar != null)
+	#assert(juicy_bar != null)
 	health_resource = resource_node.get_resource("health")
-	juicy_bar.set_default_values(0.0, health_resource.hp, health_resource.hp)
+	if juicy_bar:
+		juicy_bar.set_default_values(0.0, health_resource.hp, health_resource.hp)
 
 
 func _play_damaged()->void:
 	super._play_damaged()
-	juicy_bar.change_current_value(health_resource.hp)
+	if juicy_bar:
+		juicy_bar.change_current_value(health_resource.hp)
