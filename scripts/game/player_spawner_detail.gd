56ca7558d3d11ddd6ec007852ec07145ba8a051b
KOFLAzycat <lushoulei0552@163.com>
2025-03-29 14:06:40 +0800

    1. 增加玩家出生的特效

---
+++ scripts/game/player_spawner_detail.gd
@@ -0,0 +1,31 @@
+class_name PlayerSpawnerDetail
+extends PlayerSpawner
+
+## VFX on moment player spawns
+@export var spawn_partickle_instance_resource:InstanceResource
+
+func _ready()->void:
+	assert(player_reference != null)
+	assert(player_instance_resource != null)
+	
+	scene_transition_resource.change_scene.connect(on_scene_transition)
+	
+	if player_reference.node != null:
+		## Allow doors to register themselves.
+		on_player_scene_entry.call_deferred()
+		return
+	
+	var _partickle_config:Callable = func(inst:Node2D)->void:
+		inst.global_position = global_position
+	
+	var _config_callback:Callable = func (inst:Node2D)->void:
+		inst.global_position = Vector2(global_position.x, global_position.y - 180) 
+		var tween: Tween = create_tween()
+		tween.tween_property(inst, "global_position", global_position, 0.8).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+		await tween.finished
+		spawn_partickle_instance_resource.instance(_partickle_config)
+		# TODO 增加光束效果
+		
+	var _player:Node2D = player_instance_resource.instance(_config_callback)
+	
+	
