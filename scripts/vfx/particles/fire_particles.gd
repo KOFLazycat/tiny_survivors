dd6506794dfe75f614b313cbb3d37b747d87955a
KOFLAzycat <lushoulei0552@163.com>
2025-04-01 09:52:38 +0800

    1. 增加升级数据 2. 修复fire_particles不销毁的BUG

--- scripts/vfx/particles/fire_particles.gd
+++ scripts/vfx/particles/fire_particles.gd
@@ -2,4 +2,7 @@ class_name FireParticles
 extends GPUParticles2D
 
 func _ready():
-	set_emitting.call_deferred(true)
+	#set_emitting.call_deferred(true)
+	## 解决没有正确发送finished信号的问题
+	if !ready.is_connected(restart):
+		ready.connect(restart, CONNECT_ONE_SHOT)
