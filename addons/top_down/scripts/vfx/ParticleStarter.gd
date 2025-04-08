15504cd7035c6159ccc0b6fe8960ff0338baef2c
KOFLAzycat <lushoulei0552@163.com>
2025-04-08 18:54:49 +0800

    1. 修复粒子特效节点没有退出的BUG

--- addons/top_down/scripts/vfx/ParticleStarter.gd
+++ addons/top_down/scripts/vfx/ParticleStarter.gd
@@ -1,4 +1,7 @@
 extends GPUParticles2D
 
 func _ready()->void:
-	set_emitting.call_deferred(true)
+	#set_emitting.call_deferred(true)
+	## 解决没有正确发送finished信号的问题
+	if !ready.is_connected(restart):
+		ready.connect(restart, CONNECT_ONE_SHOT)
